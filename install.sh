0a4a7d0571ac644f2d1856137043425c83c0cff7
Clif Houck <clif@localhost.localdomain>
2016-03-22 10:20:40 -0500

    Update script to compile YouCompleteMe

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+# Currently on Fedora, but I'm known to use Debian-relatives as well, so...
+PKG_MGR="dnf"
+
 # Install RC
 cp .vimrc ~/.vimrc
 
 # Get and setup Vundle + Plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Install CMAKE, python headers for YVM
+sudo $PKG_MGR install cmake python-devel
+
+# Compile YCM
+pushd ~/.vim/bundle/YouCompleteMe/
+./install.py
+popd
